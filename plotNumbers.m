9d09d36466117af4a533b8d645b72a2488c57a97
Ines Mersak <inesmersak@gmail.com>
2017-06-03 19:12:15 +0200

    Added MATLAB script to plot points generated.

---
+++ plotNumbers.m
@@ -0,0 +1,22 @@
+filename = 'out';
+delimiterIn = ' ';
+headerlinesIn = 0;
+A = importdata(filename,delimiterIn,headerlinesIn);
+
+filename = 'web-version/out';
+delimiterIn = ' ';
+headerlinesIn = 0;
+B = importdata(filename,delimiterIn,headerlinesIn);
+
+C = rand(1000,2);
+
+scatter(A(:,1),A(:,2))
+title('My Sobol generator')
+pause
+clf
+scatter(B(101:end,1),B(101:end,2))
+title('Web Sobol generator')
+pause
+clf
+scatter(C(:,1),C(:,2))
+title('MATLAB pseudorandom')
